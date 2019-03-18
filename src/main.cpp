b5b69bee9e57561d6a0ac54fc98fb6ac405d8f92
Alexander <simple.coding.channel@gmail.com>
2019-03-18 21:54:39 +0100

    Added glfw library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main (int argc, char* argv[])
+int main(void)
 {
-	std::cout << "Hellow world" << std::endl;
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
